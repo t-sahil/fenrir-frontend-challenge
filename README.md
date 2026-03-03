8e27e979c9aaeeefced53965540ca34a75706731
t-sahil <sahilpstiwari@gmail.com>
2026-03-03 18:08:41 +0530

    chore: initial setup with vite, scss, and typescript

---
+++ README.md
@@ -0,0 +1,1 @@
+Fenrir Security - Frontend Design Challenge
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div>
+    Hello World
+    </div>
+  )
+}
+
+export default App
